a55e414713a32fa4bc340716c000a19e56ff1419
Michal <mduszyk@users.noreply.github.com>
2025-02-12 17:40:17 +0100

    updating build script

--- setup.py
+++ setup.py
@@ -2,21 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name='paramflow',
-    version='0.1.0',
+    version='0.1',
     packages=find_packages(),
-    install_requires=[
-        # any dependencies you want to include
-    ],
-    entry_points={
-        'console_scripts': [
-            'your-command=your_package.module:main_function',
-        ],
-    },
+    install_requires=[],
+    entry_points={},
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
-    url='https://github.com/yourusername/your-package',
-    author='Your Name',
-    author_email='youremail@example.com',
+    url='https://github.com/mduszyk/paramflow',
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
