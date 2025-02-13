8889c06218ae65ef7170a0ec976c4188fc83639d
Michal <mduszyk@users.noreply.github.com>
2025-02-13 07:28:58 +0100

    adding example of dqn params

--- examples/print_params.py
+++ examples/print_params.py
@@ -6,8 +6,8 @@ import paramflow as pf
 # P_FILE=params.yaml P_PROFILE=prod python print_params.py
 # python print_params.py --file params.yaml --profile=prod
 # python print_params.py --file params.toml
-# python print_params.py --file dqn_params.toml --profile dqn-adam
-# python print_params.py --file dqn_params.toml --profile dqn-adam --batch_size 64
+# python print_params.py --file dqn_train.toml --profile dqn-adam
+# python print_params.py --file dqn_train.toml --profile dqn-adam --batch_size 64
 
 params = pf.load()
 print(json.dumps(params, indent=4))
