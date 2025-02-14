b6ba798c3b80bc7cb495c07fe2111c16815db12d
Michal <mduszyk@users.noreply.github.com>
2025-02-14 10:46:47 +0100

    v0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='paramflow',
-    version='0.1.9',
+    version='0.2',
     packages=find_packages(),
     install_requires=[
         "pyyaml",
