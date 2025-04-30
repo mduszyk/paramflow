58a1e7be76e67fae57586b718c7a1e6e3f805265
Michał Duszyk <michal@duszyk.com>
2025-04-30 17:22:15 +0200

    updating version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='paramflow',
-    version='0.2.7',
+    version='0.2.8',
     description='Flexible and user-friendly parameter and configuration management library.',
     packages=find_packages(),
     install_requires=[
