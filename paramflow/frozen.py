7145ccd64afedb28c7cbd754ce40813fd0bd5487
Michał Duszyk <michal@duszyk.com>
2025-11-26 12:37:52 +0100

    fixing frozen classes error messages

--- paramflow/frozen.py
+++ paramflow/frozen.py
@@ -8,49 +8,49 @@ class ParamsDict(dict):
         object.__setattr__(self, '__dict__', self)
 
     def __setattr__(self, key, value):
-        raise AttributeError('FrozenAttrDict is immutable')
+        raise AttributeError(f'{self.__class__.__name__} is immutable')
 
     def __delattr__(self, key):
-        raise AttributeError('FrozenAttrDict is immutable')
+        raise AttributeError(f'{self.__class__.__name__} is immutable')
 
     def __setitem__(self, key, value):
-        raise TypeError('FrozenAttrDict is immutable')
+        raise TypeError(f'{self.__class__.__name__} is immutable')
 
     def __delitem__(self, key):
-        raise TypeError('FrozenAttrDict is immutable')
+        raise TypeError(f'{self.__class__.__name__} is immutable')
 
 
 class ParamsList(list):
 
     def __setitem__(self, index, value):
-        raise TypeError('FrozenList is immutable')
+        raise TypeError(f'{self.__class__.__name__} is immutable')
 
     def __delitem__(self, index):
-        raise TypeError('FrozenList is immutable')
+        raise TypeError(f'{self.__class__.__name__} is immutable')
 
     def append(self, value):
-        raise TypeError('FrozenList is immutable')
+        raise TypeError(f'{self.__class__.__name__} is immutable')
 
     def extend(self, iterable):
-        raise TypeError('FrozenList is immutable')
+        raise TypeError(f'{self.__class__.__name__} is immutable')
 
     def insert(self, index, value):
-        raise TypeError('FrozenList is immutable')
+        raise TypeError(f'{self.__class__.__name__} is immutable')
 
     def remove(self, value):
-        raise TypeError('FrozenList is immutable')
+        raise TypeError(f'{self.__class__.__name__} is immutable')
 
     def pop(self, index=-1):
-        raise TypeError('FrozenList is immutable')
+        raise TypeError(f'{self.__class__.__name__} is immutable')
 
     def clear(self):
-        raise TypeError('FrozenList is immutable')
+        raise TypeError(f'{self.__class__.__name__} is immutable')
 
     def __iadd__(self, other):
-        raise TypeError('FrozenList is immutable')
+        raise TypeError(f'{self.__class__.__name__} is immutable')
 
     def __imul__(self, other):
-        raise TypeError('FrozenList is immutable')
+        raise TypeError(f'{self.__class__.__name__} is immutable')
 
 
 def freeze(params: Union[List[any], Dict[str, any]]) -> Union[ParamsList[any], ParamsDict[str, any]]:
