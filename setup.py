b51dfd7b1d8055c180349a041005e5190930affa
Michał Duszyk <michal@duszyk.com>
2026-03-22 09:34:30 +0100

    version 0.5.9

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='paramflow',
-    version='0.5.8',
+    version='0.5.9',
     description='A lightweight library for hyperparameter and configuration management', 
     packages=find_packages(),
     install_requires=[
